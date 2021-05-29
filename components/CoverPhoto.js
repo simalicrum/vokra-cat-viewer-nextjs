e0c1c07603a8aa2303d46f8a5943657de30f689d
Robert Reinert <robert.reinert@gmail.com>
2021-05-29 09:37:45 -0700

    Added basic banner logic

--- components/CoverPhoto.js
+++ components/CoverPhoto.js
@@ -1,5 +1,18 @@
-export default function CoverPhoto({ src }) {
+import Image from 'next/image';
+
+export default function CoverPhoto({ src, cat }) {
   return (
-    <img className="text-3xl text-gray-800 md:px-4 w-full" src={src}></img>
+    <div className="md:m-4 relative">
+      {cat.Attributes.map(attribute => attribute.AttributeName).includes(
+        'No Kids'
+      ) ? (
+        <div className="absolute z-10 text-white bg-vokra-light p-1 shadow">
+          I'm Extra Special
+        </div>
+      ) : (
+        ''
+      )}
+      <Image layout="responsive" width={1000} height={1000} src={src} />
+    </div>
   );
 }
--- components/Ribbon.js
+++ components/Ribbon.js
@@ -1,5 +1,3 @@
-export default function Ribbon({children}) {
-  return (
-    <div>{children}</div>
-  );
+export default function Ribbon({ children }) {
+  return <div>{children}</div>;
 }
