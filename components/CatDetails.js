74288cb7a8327f7ca9282bcc3a10101f485d4ddf
Robert Reinert <robert.reinert@gmail.com>
2021-06-01 13:39:19 -0700

    added bg-vokra-gray in a couple of places

--- components/CatDetails.js
+++ components/CatDetails.js
@@ -4,8 +4,7 @@ import CatInfo from './CatInfo';
 export default function CatDetails({ cats, cat, returnHref, position, url }) {
   return (
     <div
-      className={`${position} inset-0 bg-white grid lg:grid-cols-2 z-20 overflow-y-auto`}
-      style={{ backgroundColor: 'rgb(245, 245, 245)' }}
+      className={`${position} inset-0 bg-white grid lg:grid-cols-2 z-20 overflow-y-auto bg-vokra-gray`}
     >
       <CatCarousel cat={cat} />
       <CatInfo cats={cats} cat={cat} returnHref={returnHref} url={url} />
